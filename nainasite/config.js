7ab1fa46597eccbe8b1923c9607a993d7ec31bae
weldonestate0 <112583948+weldonestate0@users.noreply.github.com>
2024-03-17 23:21:34 +0500

    Naina Site Added
    ajbfkj

---
+++ nainasite/config.js
@@ -0,0 +1,20 @@
+const CONFIG = {
+    titleWeb: "Naina's Site",
+    introTitle: 'Naina',
+    introDesc: `Earth is inherently strange
+    But why do I keep going the wrong way?
+    Lost in her lonely heart
+    Behind the word love is love`,
+    btnIntro: 'Hey!',
+    title: 'Maybe I fell in love at first sight 🥰',
+    desc: 'Maybe I was drunk from the moment I saw that smile ',
+    btnYes: 'I like you very much <33',
+    btnNo: 'No :3',
+    question: 'There are more than 7 billion people in the world, why do you love me <3',
+    btnReply: 'Sent to you <3',
+    reply: "If you love, you love, if you don't love, you love <33333333",
+    mess: 'I know 🥰. you love me so much 😘😘',
+    messDesc: "Saturday at 11 o'clock, I'll come pick you up and go play.",
+    btnAccept: 'Okiiiii done <3',
+    messLink: 'https://api.whatsapp.com/send?phone=923314557956&text=I%20love%20you%20sooooooo%20much%20my%20husband.' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
+}
